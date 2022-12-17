9f22364e1d7966f9bf8cf40c34b54e2f8f195f9d
homework <meremgetachew@gmail.com>
2022-12-17 23:22:17 +0300

    homework coditional

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+
+            public static void main(String[] args) {
+                Scanner input = new Scanner(System.in);
+                System.out.println("Please insert your name");
+                String name = input.next();
+                System.out.println("Hello" + name + "" + "Pleasse insert your saving amount");
+                double savingAmount = input.nextDouble();
+                if (savingAmount > 280_000) {
+                    System.out.println("Are you included on the previous registration");
+                    String previousRegistration = input.next();
+
+                    System.out.println("Do you have criminal record?");
+                    String record = input.next();
+
+                    if (record.equalsIgnoreCase("yes") && previousRegistration.equalsIgnoreCase("yes")) {
+                        System.out.println("You are in the condominium lot list");
+                    }
+
+                    if (record.equalsIgnoreCase("no") && previousRegistration.equalsIgnoreCase("no")) {
+                        System.out.println(" Congradulation! You are in the condominium lot list");
+                    } else if (record.equalsIgnoreCase("yes") && previousRegistration.equalsIgnoreCase("no")) {
+                        System.out.println("Sorry you are not in the condominium lot list");
+                    }
+                } else {
+                    System.out.println("Sorry, Your saving amount is not enough");
+                }
+
+
+            }
+        }
+
